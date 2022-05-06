e55e23787f00ca62fc9f208535cebefb35cf8cb4
root <eustache.le-bihan@imt-atlantique.net>
2022-05-06 13:17:54 +0000

    Few changes on projet file structure and thus on main.py

--- scripts/main.py
+++ scripts/main.py
@@ -6,13 +6,14 @@ import shutil
 # Création des tfrecord
 path_images = '/tf/ship_data/train_v2'
 path_to_csv = '/tf/ship_data/train_ship_segmentations_OD.csv'
-nb_images = 2500
-boat_rate = 0.5
-cut_rate = 0.5
-tfrecord_dir = '/tf/ship_detect_tl/data'
+nb_images = 53000
+boat_rate = 0.9
+cut_rate = 0.7
+tfrecord_dir = '/tf/annotations'
 
-if not os.path.exists('/tf/ship_detect_tl/scripts/test'):
-    generate_tf_record(path_images, path_to_csv, nb_images, cut_rate, boat_rate, tfrecord_dir)
+if not os.path.exists('/tf/annotations'):
+    os.mkdir('/tf/annotations')
+generate_tf_record(path_images, path_to_csv, nb_images, cut_rate, boat_rate, tfrecord_dir)
 
 # Chargement du modèle
 model_url = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d2_coco17_tpu-32.tar.gz'
